0374fc1e5f5f14d43b0a36f5d1e0068b37c6fb12
quocsu3004 <78578188+quocsu3004@users.noreply.github.com>
2021-03-01 16:37:59 +0700

    disable admin security

--- src/main/java/com/mycompany/jv40_ecommerce_boardgameshop/configuration/SecurityConfig.java
+++ src/main/java/com/mycompany/jv40_ecommerce_boardgameshop/configuration/SecurityConfig.java
@@ -39,10 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/", "/login", "/logout", "/home").permitAll();
+        http.authorizeRequests().antMatchers("/", "/login", "/logout", "/home","/admin").permitAll();
 
-        http.authorizeRequests().antMatchers("/user/*").access("hasAnyRole('ROLE_ADMIN,ROLE_USER')")
-                .antMatchers("/admin/*").access("hasRole('ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/user/*").access("hasAnyRole('ROLE_ADMIN,ROLE_USER')");
+//                .antMatchers("/admin/*").access("hasRole('ROLE_ADMIN')");
 
         http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
 
